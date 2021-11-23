378bc0f2151c6db6f8d8065528acfb16dc2bc90a
Amanda DiNoto <amanda.dinoto@gmail.com>
2021-11-23 13:22:45 -0500

    add authenticaton and authorization

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-  return HttpResponse('<h1>Welcome</h1>')
+from main_app.models import Remedies
+
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
+
+# class RemedyCreate(LoginRequiredMixin, CreateView):
+#   def form_valid(self, form):
+#     form.instance.user = self.request.user 
+#     return super().form_valid(form)
+
+# @login_required
+# def cats_index(request):
+#   remedies = Remedy.objects.filter(user=request.user)
+#   return render(request, 'cats/index.html', { 'remedies': remedies })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('herbs_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
